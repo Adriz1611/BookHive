781f07b4c45d28f4c30922d367a41d732f90433b
unknown <priyankamay42@gmail.com>
2025-01-18 19:40:03 +0530

    added ownersRouter

--- app.js
+++ app.js
@@ -19,4 +19,8 @@ app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 
+app.get("/", (req, res) => {
+    res.send("hey");
+});
+
 app.listen(3000);
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
 
 router.get("/", (req, res) => {
-  res.send("hello");
+    res.send("hello");
 });
 
+
+ router.post("/create", async(req,res)=>{
+        let owners= await ownerModel.find();
+        if( owners.length>0)
+        {
+            res.send("owner already exists");  
+        }
+
+        let {fullname,email,password}=req.body;
+
+        let createdOwner= await ownerModel.create({
+            fullname,
+            email,
+            password,
+        });
+
+        res.status(201).send(createdOwner);
+
+    })
+
+    
+
+
+
+    
+    
 module.exports = router;
