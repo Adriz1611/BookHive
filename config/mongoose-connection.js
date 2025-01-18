c9afdf151e2b76e5207843195b44b073c54227f3
unknown <priyankamay42@gmail.com>
2025-01-18 17:45:54 +0530

    added debugger

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const config = require("config");
+const dbgr=require("debug")("development:mongoose");
 
 mongoose
   .connect(`${config.get("MONGODB_URI")}/bookhive`)
   .then(function () {
-    console.log("connected");
+    dbgr("connected");
   })
   .catch(function (err) {
-    console.log(err);
+    dbgr(err);
   });
 
 module.exports = mongoose.connection;
