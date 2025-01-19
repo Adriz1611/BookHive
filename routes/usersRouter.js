1b47b453f5e58577907cb20c080eaead6b1bf3fe
unknown <priyankamay42@gmail.com>
2025-01-19 19:23:00 +0530

    setting up usersRouter

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const isloggedin = require("../middlewares/isLoggedIn");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
 
-router.get("/",(req,res)=>{
-    res.send("heyyo");
+router.get("/", function (req, res) {
+  res.send("hey it's working");
 });
 
+router.post("/register", registerUser);
 
-router.post("/register", (req, res) => {
-  res.send("heyo bitch");
-});
+router.post("/login", loginUser);
 
+router.get("/logout", logout);
 
-module.exports=router;
+module.exports = router;
