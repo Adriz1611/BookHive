183f54d22cda45007feabf9867227ff3e35dfb63
unknown <priyankamay42@gmail.com>
2025-01-24 10:27:21 +0530

    setting up owner/admin page

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,28 +2,30 @@ const express = require("express");
 const router = express.Router();
 const ownerModel = require("../models/owner-model");
 
-router.get("/", (req, res) => {
-    res.send("hello");
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .status(500)
+        .send("You don't have permission to create a new owner.");
+    }
+
+    let { fullname, email, password } = req.body;
+
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+
+    res.status(201).send(createdOwner);
+  });
+}
+
+router.get("/", function (req, res) {
+  res.render("owner-login");
 });
 
-
- router.post("/create", async(req,res)=>{
-        let owners= await ownerModel.find();
-        if( owners.length>0)
-        {
-            res.send("owner already exists");  
-        }
-
-        let {fullname,email,password}=req.body;
-
-        let createdOwner= await ownerModel.create({
-            fullname,
-            email,
-            password,
-        });
-
-        res.status(201).send(createdOwner);
-
-    })
-    
 module.exports = router;
+
